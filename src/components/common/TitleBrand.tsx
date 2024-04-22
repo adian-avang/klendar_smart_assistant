df41deb356bc2fc5654bc9cdaabccc0dc8bb183c
adian-avang <adian.cm@avangenio.com>
2024-04-22 09:26:49 -0400

    goto and back from chat

--- src/components/common/TitleBrand.tsx
+++ src/components/common/TitleBrand.tsx
@@ -1,7 +1,7 @@
 const TitleBrand = () => {
   return (
     <>
-      <h1 className='text-xl text-nowrap text-white font-extrabold px-4'>
+      <h1 className='text-xl text-nowrap text-white font-extrabold'>
         K-lendar
       </h1>
     </>
